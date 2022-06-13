e7ff782a41d6ab19cb357720c9712985ee59ade9
Felipe Andrade <felipe_fac_8@hotmail.com>
2022-06-13 15:21:47 -0300

    create signup screen

---
+++ src/screens/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+export function SignUp() {
+  return <TouchableWithoutFeedback></TouchableWithoutFeedback>;
+}
