748e5b86ba21d400511cd49c07ea3542ef1dc32b
Felipe Andrade <felipe_fac_8@hotmail.com>
2022-06-13 23:10:18 -0300

    add font rubik

--- App.tsx
+++ App.tsx
@@ -1,8 +1,76 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { SignUp } from "./src/screens/SignUp";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_500Medium,
+  Rubik_600SemiBold,
+  Rubik_700Bold,
+  Rubik_400Regular_Italic,
+  Rubik_500Medium_Italic,
+  Rubik_600SemiBold_Italic,
+  Rubik_700Bold_Italic,
+} from "@expo-google-fonts/rubik";
+import { View } from "react-native";
 
 export default function App() {
-  return <SignUp />;
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          Rubik_400Regular,
+          Rubik_500Medium,
+          Rubik_600SemiBold,
+          Rubik_700Bold,
+          Rubik_400Regular_Italic,
+          Rubik_500Medium_Italic,
+          Rubik_600SemiBold_Italic,
+          Rubik_700Bold_Italic,
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <View
+      onLayout={onLayoutRootView}
+      style={{
+        flex: 1,
+      }}
+    >
+      <SignUp />
+    </View>
+  );
 }
--- src/screens/SignUp/styles.ts
+++ src/screens/SignUp/styles.ts
@@ -4,4 +4,5 @@ export const Container = styled.View`
   flex: 1;
   background-color: #f1f1f1;
   padding: 24px 0;
+  font-family: "Rubik_500Medium";
 `;
